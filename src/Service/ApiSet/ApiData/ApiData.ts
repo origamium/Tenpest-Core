efd7aa41dcf952fc1efc3ff9581c9b17aee19638
on_origami <yonosamochi@me.com>
2018-04-29 13:12:36 +0900

    ApiDataにHTTP Methodのことを追加

--- src/Service/ApiSet/ApiData/ApiData.ts
+++ src/Service/ApiSet/ApiData/ApiData.ts
@@ -3,5 +3,6 @@ import {ApiParameter} from "./ApiParameter";
 export interface ApiData {
     path: string;
     parameter: ApiParameter;
+    method: "GET" | "HEAD" | "POST" | "PUT" | "DELETE" | "CONNECT" |"PATCH";
     return: string;
 }
