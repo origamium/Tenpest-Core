d5ba078b73cfed954de4441a83ae305473c27534
on_origami <yonosamochi@me.com>
2018-04-29 17:53:44 +0900

    puppeteerの導入とjestでpuppeteerでテスト出来るように

--- jest.config.js
+++ jest.config.js
@@ -5,4 +5,7 @@ module.exports = {
     testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
     moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
     collectCoverage: true,
+    globalSetup: './jest/setup.js',
+    globalTeardown: './jest/teardown.js',
+    testEnvironment: './jest/puppeteer_env.js'
 };
