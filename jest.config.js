08a62fbae844de9dfc19b3a746443690b1b8be37
on_origami <yonosamochi@me.com>
2018-04-30 14:21:38 +0900

    puppeteerを一時的に解除

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,7 @@ module.exports = {
     testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
     moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
     collectCoverage: true,
-    globalSetup: './jest/setup.js',
+    /*globalSetup: './jest/setup.js',
     globalTeardown: './jest/teardown.js',
-    testEnvironment: './jest/puppeteer_env.js'
+    testEnvironment: './jest/puppeteer_env.js'*/
 };
