62a49d4183bb797b87a751351d7506cede0c2759
ONOUE_Origami <onoueorigami@gmail.com>
2018-05-05 16:26:20 +0900

    ApiDataにおいてReturnedDataを使用するように定義を変更

--- src/Service/ApiSet/ApiData/ApiData.ts
+++ src/Service/ApiSet/ApiData/ApiData.ts
@@ -1,9 +1,10 @@
-import {ApiParameter} from "./ApiParameter";
-import {HttpMethods} from "./HttpMethods";
+import {ApiParameter} from './ApiParameter';
+import {HttpMethods} from './HttpMethods';
+import {ReturnedDatumInfo} from './returnedDatum';
 
 export interface ApiData {
     path: string;
     parameter: ApiParameter;
     method: HttpMethods;
-    return: string;
+    return: ReturnedDatumInfo | null;
 }
