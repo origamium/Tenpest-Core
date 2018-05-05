dd3f38671b01a26fd0643bfa138e35d84dd41702
ONOUE_Origami <onoueorigami@gmail.com>
2018-05-05 16:05:11 +0900

    帰ってくるデータ形式についてあれするのを追加

---
+++ src/Service/ApiSet/ApiData/returnedDatum.ts
@@ -0,0 +1,14 @@
+export interface ReturnedDatumInfo {
+    /*
+     *  default: null. In this state, checking HTTP Status Code.
+     *  e.g. Slack is always return 200 OK, error checking must be see "ok" parameter.
+    */
+    extendErrorCheck: null | string;
+
+    /*
+    * default: null. In this state, target data is received json/xml data.
+    * e.g. Slack is always contain "ok" parameter in return data, and It always changing target data by request api.
+    * F*CKING SLACK API!
+    * */
+    targetParameterName: null | string;
+}
