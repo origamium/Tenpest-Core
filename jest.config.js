1cf201bece439ffe3d542fc20cb279d9b0c3220f
ONOUE_Origami <onoueorigami@gmail.com>
2018-05-05 14:18:41 +0900

    TypescriptファイルをテストするのではなくコンパイルされたTypescriptファイルをテストするように変更

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,4 @@
 module.exports = {
-    transform: {
-        "^.+\\.(ts|tsx)$": "<rootDir>/preprocessor.js",
-    },
-    testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
-    moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
     collectCoverage: true,
     globalSetup: './jest/setup.js',
     globalTeardown: './jest/teardown.js',
