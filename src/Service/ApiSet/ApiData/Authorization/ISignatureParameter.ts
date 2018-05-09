d7facb3cc36f6a3d545982a14976c4736fea1628
ONOUE Origami <yonosamochi@me.com>
2018-05-10 04:11:43 +0900

    Signature生成に必要なパラメータを定義

---
+++ src/Service/ApiSet/ApiData/Authorization/ISignatureParameter.ts
@@ -0,0 +1,10 @@
+import {SignType} from "./SignType";
+
+export interface ISignatureParameter {
+    oauth_consumer_key: string;
+    oauth_token?: string;
+    oauth_signature_method: SignType;
+    oauth_timestamp: string | number;
+    oauth_nonce: string;
+    oauth_version: string;
+}
