1a70bd50dd527b9bc8cf43cd2370a2940da71fa7
on_origami <yonosamochi@me.com>
2018-05-10 18:44:42 +0900

    いまふたたびのts-jestを適用

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,9 @@
 module.exports = {
+    transform: {
+        "^.+\\.tsx?$": "ts-jest",
+    },
+    testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
+    moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
     collectCoverage: true,
     globalSetup: './jest/setup.js',
     globalTeardown: './jest/teardown.js',
