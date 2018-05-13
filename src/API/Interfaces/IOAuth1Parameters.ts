777fae96122e7213f0b0b393e87b0d5b5168b4a2
ONOUE Origami <yonosamochi@me.com>
2018-05-14 05:57:54 +0900

    ISignatureParametersをIOAuth1Parameterにリネーム

--- src/API/Interfaces/IOAuth1Parameters.ts
+++ src/API/Interfaces/IOAuth1Parameters.ts
@@ -1,6 +1,6 @@
 import {SignType} from "../Enums/SignType";
 
-export interface ISignatureParameter {
+export interface IOAuth1Parameters {
     oauth_consumer_key: string;
     oauth_token?: string;
     oauth_signature_method: SignType;
