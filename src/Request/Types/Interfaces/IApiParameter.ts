b68e7052fd895f2d41479b253519cf5271ca7bb6
ONOUE Origami <yonosamochi@me.com>
2018-05-15 11:56:41 +0900

    重複！削除！

--- src/Request/Types/Interfaces/IApiParameter.ts
+++
@@ -1,8 +0,0 @@
-import {ApiParameterType} from '../Enums/ApiParameterType';
-
-export interface IApiParameter {
-    [key: string]: {
-        required: boolean,
-        type: ApiParameterType,
-    };
-}
