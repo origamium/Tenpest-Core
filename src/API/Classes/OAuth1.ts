5754d77b4a60624fe7c0120a096289dd18ec11dd
ONOUE Origami <yonosamochi@me.com>
2018-05-15 11:17:42 +0900

    signature生成器をOAuth1に追加

--- src/API/Classes/OAuth1.ts
+++ src/API/Classes/OAuth1.ts
@@ -1,20 +1,33 @@
-import generateSignature from '../Functions/signature/generateSignature';
+import * as authSign from 'oauth-sign';
 import OAuth from "./OAuth";
 import {HttpMethods} from "../Enums/HttpMethods";
 import {AuthMethods} from "../Enums/AuthMethods";
+import {IOAuth1Parameters} from "../Interfaces/IOAuth1Parameters";
 
 export default class OAuth1 extends OAuth {
     constructor() {
         super();
-
     }
 
     static _now(): number {
         return Math.round(+new Date()/1000);
     }
 
-    static _signature(httpMethod: HttpMethods): string {
-
+    static _signature(
+        httpMethod: HttpMethods,
+        baseUrl: string,
+        parameter: IOAuth1Parameters,
+        queryParameter: object,
+        consumerSecretKey: string,
+        tokenSecret: string = '',) : string {
+        return encodeURIComponent(authSign.sign(
+            parameter.oauth_signature_method,
+            httpMethod,
+            baseUrl,
+            {...parameter, ...queryParameter},
+            consumerSecretKey,
+            tokenSecret,
+        ));
     }
 
     pinAuthorization(method: AuthMethods): void {
