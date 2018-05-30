13dcde5a99f8385dd1ee0367c9b318c67b8e7b32
ONOUE Origami <yonosamochi@me.com>
2018-05-31 06:57:35 +0900

    UnitにHTTP Methodを保管する欄を忘れていた

--- src/API/Unit.ts
+++ src/API/Unit.ts
@@ -1,13 +1,19 @@
 import {IReturnedDatumInfo} from '../Service/ApiSet/ApiData/IReturnedDatumInfo';
 import {IApiParameterDefinition} from '../Authorization/Interfaces/IApiParameterDefinition';
+import {HttpMethods} from '../Authorization/Enums/HttpMethods';
 
 export class Unit {
     private readonly _path: string;
+    private readonly _httpMethod: HttpMethods;
     private readonly _returnDatum: IReturnedDatumInfo;
     private readonly _parameter: IApiParameterDefinition;
 
     get path(): string {
         return this._path;
+    }
+
+    get httpMethod(): HttpMethods {
+        return this._httpMethod;
     }
 
     get returnDatum(): IReturnedDatumInfo {
@@ -18,8 +24,9 @@ export class Unit {
         return this._parameter;
     }
 
-    constructor(path: string, returnDatum: IReturnedDatumInfo, parameter: IApiParameterDefinition) {
+    constructor(path: string, httpMethod: HttpMethods, returnDatum: IReturnedDatumInfo, parameter: IApiParameterDefinition) {
         this._path = path;
+        this._httpMethod = httpMethod;
         this._returnDatum = returnDatum;
         this._parameter = parameter;
     }
