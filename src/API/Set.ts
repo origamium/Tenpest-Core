2aabe9169a64616c57a6d359a778f4e873f7af70
ONOUE Origami <yonosamochi@me.com>
2018-06-03 06:24:39 +0900

    曖昧にSetクラスを定義

--- src/API/Set.ts
+++ src/API/Set.ts
@@ -4,6 +4,25 @@
 * └───────────┘
 */
 
+import {IChannel} from './Category/IChannel';
+import {IUser} from './Category/IUser';
+import {IDirectMessage} from './Category/IDirectMessage';
+import {IAccount} from './Category/IAccount';
+import {IContentList} from './Category/IContentList';
+
 export class Set {
-    // TODO
+    private readonly _authorization: object; // TODO
+    private readonly _account?: IAccount;
+    private readonly _user?: IUser;
+    private readonly _channel?: IChannel;
+    private readonly _contentList?: IContentList;
+    private readonly _directMessage?: IDirectMessage;
+
+    constructor() {
+        // TODO
+    }
+
+    public get availableCategoryKeys(): string[] {
+        return Object.keys(Object.assign(this));
+    }
 }
