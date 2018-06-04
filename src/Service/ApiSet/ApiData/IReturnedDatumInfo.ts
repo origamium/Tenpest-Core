e8182b5e140b7129a1dc0d08ce17032f918a0dc2
on_origami <yonosamochi@me.com>
2018-06-04 18:59:53 +0900

    便利

--- src/Service/ApiSet/ApiData/IReturnedDatumInfo.ts
+++ src/Service/ApiSet/ApiData/IReturnedDatumInfo.ts
@@ -3,12 +3,14 @@ export interface IReturnedDatumInfo {
      *  default: null. In this state, checking HTTP Status Code.
      *  e.g. Slack is always return 200 OK, error checking must be see "ok" parameter.
     */
-    extendErrorCheck: null | string;
+    extendErrorCheck?: string;
 
     /*
     * default: null. In this state, target data is received json/xml data.
     * e.g. Slack is always contain "ok" parameter in return data, and It always changing target data by request api.
     * F*CKING SLACK API!
     * */
-    targetParameterName: null | string;
+    targetParameterName?: string;
+
+    body: object | object[];
 }
