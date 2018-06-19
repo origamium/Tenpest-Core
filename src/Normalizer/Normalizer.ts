b9217011b41480a6a76cd624fa4cd9f8308950aa
on_origami <yonosamochi@me.com>
2018-06-19 18:35:20 +0900

    Normalizerのがわだけ作成

---
+++ src/Normalizer/Normalizer.ts
@@ -0,0 +1,7 @@
+import {Unit} from '../API/Unit';
+import normalize from './normalize';
+import smooth from './smooth';
+
+export default (unitdata: Unit, data: object | object[]): object => {
+    return {};
+};
