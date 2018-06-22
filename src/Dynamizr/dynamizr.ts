e90bd2294ab6c2580147c44cc3745eb6a0f75a24
ONOUE Origami <yonosamochi@me.com>
2018-06-22 20:31:51 +0900

    dynamizrを一時的に無効に

--- src/Dynamizr/dynamizr.ts
+++
@@ -1,6 +0,0 @@
-import normalizer from './Functions/normalize';
-import transformer from './Functions/transform';
-
-export default (schema, data) => (
-    transformer(schema, normalizer(schema, data))
-);
