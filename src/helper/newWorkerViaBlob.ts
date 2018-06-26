af1d05aa85278296fb8aea79b38403d6cdbea6c6
on_origami <yonosamochi@me.com>
2018-06-26 15:16:23 +0900

    不要になってしまったhelperとinterfaceを削除

--- src/helper/newWorkerViaBlob.ts
+++
@@ -1,9 +0,0 @@
-export default (relativePath: string): Worker => {
-    const baseURL = window.location.href
-        .replace(/\\/g, '/')
-        .replace(/\/[^\/]*$/, '/');
-    const array = ['importScripts("' + baseURL + relativePath + '");'];
-    const blob = new Blob(array, {type: 'text/javascript'});
-    const url = window.URL.createObjectURL(blob);
-    return new Worker(url);
-};
