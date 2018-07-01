6e879e9b6fee7ade335f7dae3804c54fc56ae46e
on_origami <yonosamochi@me.com>
2018-07-01 20:12:17 +0900

    ProviderObjectを定義

--- src/SavedObjectTypes/Provider/ProviderObject.ts
+++ src/SavedObjectTypes/Provider/ProviderObject.ts
@@ -1,7 +1,11 @@
 import {PairOfObject} from '../PairOfObject';
 
 export type ProviderObject = {
-
+    serviceName: string,
+    domain: string, // domain must be equal key
+    apiKey: string,
+    apiSecret: string,
 };
 
+// key is domain. e.g. 'twitter.com', 'slack.com'
 export type Providers = PairOfObject<ProviderObject>;
