e3b712ce926795a2ddc9553cdfc902d73b02c864
on_origami <yonosamochi@me.com>
2018-07-01 20:25:15 +0900

    微妙な変更

--- src/Provider/Provider.ts
+++ src/Provider/Provider.ts
@@ -1,13 +1,19 @@
 import { IAPIKey } from '../Authorization/Interfaces/IKeys';
+import Exportable from '../helper/Exportable';
 
-export default class Provider {
+export default class Provider extends Exportable {
     private _baseUrl: string; // https://slack.com/api/, https://api.twitter.com/, https://mstdn.jp/api/v1/ ...
     private readonly _domain: string; // mstdn.jp, pawoo.net...
     private _apiKey: IAPIKey;
 
     constructor(baseUrl: string, domain: string, apikey: IAPIKey) {
+        super();
         this._baseUrl = baseUrl;
         this._domain = domain;
         this._apiKey = apikey;
+    }
+
+    public export() {
+        return {};
     }
 }
