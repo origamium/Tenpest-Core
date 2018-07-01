ef1051e67df054ec63715dd8fb53ef07e3983433
on_origami <yonosamochi@me.com>
2018-07-01 18:02:49 +0900

    WebWorker上ではなくNode.js上で動くように存在が変更されたためPuppeteerをテスト環境から削除

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,4 @@ module.exports = {
     testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
     moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
     collectCoverage: true,
-    globalSetup: './jest/setup.js',
-    globalTeardown: './jest/teardown.js',
-    testEnvironment: './jest/puppeteer_env.js',
 };
