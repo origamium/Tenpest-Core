f38b3f3185c4fe66f56bbc14d84f62db74981833
on_origami <yonosamochi@me.com>
2018-07-01 20:20:14 +0900

    ServiceObjectを整備 不完全

--- src/SavedObjectTypes/Service/ServiceObject.ts
+++ src/SavedObjectTypes/Service/ServiceObject.ts
@@ -1,7 +1,10 @@
 import {PairOfObject} from '../PairOfObject';
 
 export type ServiceObject = {
-
+    serviceName: string, // is equal key
+    apiSet: object,
+    dataSet: object,
 };
 
+// key is Service Name. e.g. 'twitter', 'slack', 'mastodon'.
 export type Services = PairOfObject<ServiceObject>;
