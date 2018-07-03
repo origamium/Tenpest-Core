0442a499cca8ebc6178d26f9358c42835f7d593d
on_origami <yonosamochi@me.com>
2018-07-03 18:53:47 +0900

    Account/AuthorizationData classの整備

--- src/Account/AuthorizationData.ts
+++ src/Account/AuthorizationData.ts
@@ -1,15 +1,40 @@
-export default class AuthorizationData {
-    private _token: string;
+import {AuthorizationDataObject} from '../SavedObjectTypes/Account/AuthorizationDataObject';
+import Exportable from '../helper/Exportable';
 
+export default class AuthorizationData extends Exportable {
+    private _token: string;
     private _tokenSecret?: string;
 
-    private _refreshToken?: string;
-    private _tokenAcquisitionTime?: string;
-    private _tokenExpire?: string;
+    private _refreshTokenObject?: {
+        refreshToken: string,
+        tokenAcquisitionDate: Date,
+        tokenExpireDate: Date,
+    };
     private _tokenIsExprired?: boolean;
 
-    constructor(authData: any) { // TODO: DO NOT ANY
-        this._token = authData.token;
-        this._tokenSecret = authData.tokenSecret;
+    constructor(source: AuthorizationDataObject) {
+        super();
+        this._token = source.token;
+        this._tokenSecret = source.tokenSecret;
+        if (source.refreshTokenObject) {
+            this._refreshTokenObject = {
+                refreshToken: source.refreshTokenObject.refreshToken,
+                tokenAcquisitionDate: new Date(source.refreshTokenObject.tokenAcquisitionDate),
+                tokenExpireDate: new Date(source.refreshTokenObject.tokenExpireDate),
+            };
+            this._tokenIsExprired =  source.refreshTokenObject.tokenExpireDate <= Date.now();
+        }
+    }
+
+    public export(): AuthorizationDataObject {
+        return {
+            token: this._token,
+            tokenSecret: this._tokenSecret,
+            refreshTokenObject: this._refreshTokenObject ? {
+                refreshToken: this._refreshTokenObject.refreshToken,
+                tokenAcquisitionDate: this._refreshTokenObject.tokenAcquisitionDate.getTime(),
+                tokenExpireDate: this._refreshTokenObject.tokenExpireDate.getTime(),
+            } : undefined,
+        };
     }
 }
