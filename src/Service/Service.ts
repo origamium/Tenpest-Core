693c84370f35797311ae375933adac1876678987
on_origami <yonosamochi@me.com>
2018-07-05 01:51:25 +0900

    Service クラスを適当に作成

--- src/Service/Service.ts
+++ src/Service/Service.ts
@@ -1,2 +1,21 @@
-export default class Service {
+import Exportable from '../helper/Exportable';
+import {ServiceObject} from '../SavedObjectTypes/Service/ServiceObject';
+import {APISet} from '../API/APISet';
+
+export default class Service extends Exportable {
+    private readonly _serviceName: string;
+    private readonly _apiSet: APISet;
+    private readonly _dataSet: object; // TODO
+    private readonly _uiSet: object; // TODO
+
+    constructor(source: ServiceObject) {
+        super();
+
+    }
+
+    public export(): ServiceObject {
+        return {
+
+        };
+    }
 }
