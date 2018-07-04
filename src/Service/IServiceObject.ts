546b2b763a7c73f78df75ee33bae7d40e15450e3
on_origami <yonosamochi@me.com>
2018-07-04 19:54:54 +0900

    よくわからないのを削除

--- src/Service/IServiceObject.ts
+++
@@ -1,8 +0,0 @@
-import { Set } from '../API/Set';
-import Provider from '../Provider/Provider';
-
-export interface IServiceObject {
-    serviceName: string;
-    apiSet: Set;
-    provider: Provider | Provider[];
-}
