d1602b602b34139efaec1453311fbc88e86024bd
on_origami <yonosamochi@me.com>
2018-07-05 02:10:19 +0900

    Provider classを雑に整備

--- src/Provider/Provider.ts
+++ src/Provider/Provider.ts
@@ -1,19 +1,19 @@
-import { IAPIKey } from '../Authorization/Interfaces/IKeys';
-import Exportable from '../helper/Exportable';
+import { IAPIKey } from '../API/Authorization/Interfaces/IKeys';
+import {ProviderObject} from '../SavedObjectTypes/Provider/ProviderObject';
 
-export default class Provider extends Exportable {
-    private _baseUrl: string; // https://slack.com/api/, https://api.twitter.com/, https://mstdn.jp/api/v1/ ...
+export default class Provider {
+    private readonly _name: string;
+    private readonly _baseUrl: string; // https://slack.com/api/, https://api.twitter.com/, https://mstdn.jp/api/v1/ ...
     private readonly _domain: string; // mstdn.jp, pawoo.net...
-    private _apiKey: IAPIKey;
+    private readonly _apiKey: IAPIKey;
 
-    constructor(baseUrl: string, domain: string, apikey: IAPIKey) {
-        super();
-        this._baseUrl = baseUrl;
-        this._domain = domain;
-        this._apiKey = apikey;
-    }
-
-    public export() {
-        return {};
+    constructor(source: ProviderObject) {
+        this._name = source.providerName;
+        this._baseUrl = source.baseUrl;
+        this._domain = source.domain;
+        this._apiKey = {
+            ApiKey: source.apiKey,
+            ApiSecretKey: source.apiSecret,
+        };
     }
 }
