637decc62e4b7f4d1b915680d81a38bc062b8e74
on_origami <yonosamochi@me.com>
2018-07-07 00:26:02 +0900

    ServiceをExportableではなくした

--- src/Service/Service.ts
+++ src/Service/Service.ts
@@ -2,20 +2,13 @@ import Exportable from '../helper/Exportable';
 import {ServiceObject} from '../SavedObjectTypes/Service/ServiceObject';
 import {APISet} from '../API/APISet';
 
-export default class Service extends Exportable {
+export default class Service {
     private readonly _serviceName: string;
     private readonly _apiSet: APISet;
     private readonly _dataSet: object; // TODO
     private readonly _uiSet: object; // TODO
 
     constructor(source: ServiceObject) {
-        super();
 
-    }
-
-    public export(): ServiceObject {
-        return {
-
-        };
     }
 }
