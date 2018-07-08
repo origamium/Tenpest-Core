ea86b31fd1c2eaade8e908d0a9369ee77752d8ab
ONOUE Origami <onoueorigami@gmail.com>
2018-07-08 18:28:59 +0900

    ApiSetObjectにstreaming, rcpを追加

--- src/SavedObjectTypes/Service/ApiSet/ApiSetObject.ts
+++ src/SavedObjectTypes/Service/ApiSet/ApiSetObject.ts
@@ -4,5 +4,7 @@ import {AuthorizationUnitObject} from './AuthorizationUnitObject';
 export type ApiSetObject = {
     authorization: AuthorizationUnitObject,
     rest: RestApiUnits,
+    streaming?: object, // HTTP Streaming API. // TODO
     rtc?: object, // real-time connection. // TODO
+    rcp?: object, // remote call procedure api. // TODO
 };
