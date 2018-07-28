c4cca992d6060f6b9ffb6fb77f2e2d229e1ef3d8
ONOUE Origami <onoueorigami@gmail.com>
2018-07-29 02:13:23 +0900

    ServiceObjectに認証情報を追加

--- src/StoredObjectTypes/Service/ServiceObject.ts
+++ src/StoredObjectTypes/Service/ServiceObject.ts
@@ -1,11 +1,20 @@
 import {PairOfObject} from '../../helper/PairOfObject';
 import {ApiSetObject} from './ApiSet/ApiSetObject';
 import {DataSets} from './DataSet/DataSetObject';
+import {OAuthVersion} from '../../Enums/OAuthVersion';
+import {SignMethod} from '../../Enums/SignMethod';
+import {SignSpace} from '../../Enums/SignSpace';
+import {AuthMethods} from '../../Enums/AuthMethods';
 
 export type ServiceObject = {
     serviceName: string, // is equal key
     apiSet: ApiSetObject,
     dataSet: DataSets,
+
+    authMethod: OAuthVersion,
+    signMethod: SignMethod,
+    signSpace: SignSpace,
+    certMethod: AuthMethods,
 };
 
 // key is Service Name. e.g. 'twitter', 'slack', 'mastodon'.
