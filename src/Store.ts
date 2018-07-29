4df68e2a986d0cfce068d8c487aca29b937df9ef
ONOUE Origami <yonosamochi@me.com>
2018-07-30 02:01:03 +0900

    ストアの設計を一旦変更

--- src/Store.ts
+++ src/Store.ts
@@ -1,8 +1,8 @@
 import Account from './Account/Account';
 import {PairOfObject} from './helper/PairOfObject';
 import Provider from './Provider/Provider';
-import {RootObject} from './StoredObjectTypes/RootObject';
 import Service from './Service/Service';
+import {RootObject} from './StoredObjectTypes/RootObject';
 
 export default class Store {
     private providers: PairOfObject<Provider> = {};
@@ -13,45 +13,11 @@ export default class Store {
         // TODO
     }
 
-    public _load(loadedObject: RootObject): boolean {
-        /*
-        * 今の所、DefaultSupportをloadedObjectに渡してテストしていますが、
-        * DefaultSupportを読み込んでからJSONファイルを読む形に直してください。
-        */
-        let loadIsSuccessed = true;
+    public _defaultLoad(loadedObject: RootObject) {
 
-        Object.keys(loadedObject.services).forEach((serviceKey) => {
-            try {
-                this.services[serviceKey] = new Service(loadedObject.services[serviceKey]);
+    }
 
-                Object.keys(loadedObject.providers)
-                    .filter((providerKey) => serviceKey === loadedObject.providers[providerKey].serviceKey)
-                    .forEach((providerKey) => {
-                    try {
-                        this.providers[providerKey] = new Provider(loadedObject.providers[providerKey]);
-                    } catch (e) {
-                        console.error('failed load provider object.');
-                        console.error(e.toString());
-                        loadIsSuccessed = false;
-                    }
-                });
-            } catch (e) {
-                console.error('failed load service object.');
-                console.error(e.toString());
-                loadIsSuccessed = false;
-            }
-        });
+    public _load(loadedObject: RootObject) {
 
-        Object.keys(loadedObject.accounts).forEach((key) => {
-            try {
-                this.accounts[key] = new Account(loadedObject.accounts[key]);
-            } catch (e) {
-                console.error('failed load account object.');
-                console.error(e.toString());
-                loadIsSuccessed = false;
-            }
-        });
-
-        return loadIsSuccessed;
     }
 }
