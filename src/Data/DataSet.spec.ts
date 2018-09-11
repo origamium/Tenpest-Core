e6ff2c542aea15f6733229124e4b2b66055cbb0c
Origami ONOUE <onoueorigami@gmail.com>
2018-09-12 01:01:29 +0900

    DataSetのテストを機能している状態に

--- src/Data/DataSet.spec.ts
+++ src/Data/DataSet.spec.ts
@@ -5,7 +5,13 @@ import Twitter from '../DefaultSupport/Twitter';
 import * as data from '../testdata/twitter/home_timeline.json';
 const instance = new DataSet(Twitter.service.dataSet);
 
-test('dummy', () => {
-    console.log(instance.normalize('statusList', data));
-    expect('dummy').toBe('dummy');
+describe('dynamizr working tests', () => {
+    test('exactly data normalize', () => {
+        const result = instance.normalize('statusList', data);
+        expect(result.entities).toHaveProperty('users');
+        expect(result.entities).toHaveProperty('contents');
+        expect(result.result).toEqual(['240558470661799936', '240556426106372096', '240539141056638977']);
+    });
 });
+
+
