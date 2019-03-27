f2db56fa0697f9098cf11f9b1adc6bc7ed7b3056
Origami ONOUE <onoueorigami@gmail.com>
2019-03-27 20:50:27 +0900

    はい

--- src/StoredObjectTypes/Service/DataSet/SchemaObject.ts
+++ src/StoredObjectTypes/Service/DataSet/SchemaObject.ts
@@ -1,11 +1,12 @@
 import {schemaTypes} from '../../../Enums/schemaTypes';
 import {PairOfObject} from '../../../helper/PairOfObject';
+import {ITransform} from '../../../Data/Dynamizr/Interfaces/ITransform';
 
 export type SchemaObjectElement = {
     name: string, // equal key
     type: schemaTypes,
     idAttribute: string,
-    transform: object,
+    transform: ITransform,
     definition?: PairOfObject<SchemaObject>,
 };
 
